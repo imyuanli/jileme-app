cfa8d3081ace7a99fc90bb368f2f6f513f0fdd82
imyuanli <2865437316@qq.com>
2026-08-20 17:53:39 +0800

    feat(layout): implement theme provider and add UI components
    - Integrate ThemeProvider with NAV_THEME based on color scheme
    - Add StatusBar with dynamic style based on color scheme
    - Prevent auto hide of splash screen until app is ready
    - Add Accordion, Alert, AlertDialog, AspectRatio, Avatar, Badge components
    - Update Button component with new styling and variants
    - Add Card, Checkbox, Collapsible, ContextMenu components
    - Implement Dialog component with overlay and content styling
    - Update index page with new UI elements and layout classes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,21 @@
-import { Stack } from "expo-router"
-import { PortalHost } from "@rn-primitives/portal"
-import { View } from "react-native"
 import "@/global.css"
+import { Stack, ThemeProvider } from "expo-router"
+import { PortalHost } from "@rn-primitives/portal"
+import * as SplashScreen from "expo-splash-screen"
+import { useColorScheme } from "react-native"
+import { NAV_THEME } from "@/lib/theme"
+import { StatusBar } from "expo-status-bar"
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const colorScheme: "light" | "dark" | "unspecified" = useColorScheme() || "light"
+
   return (
-    <View>
+    <ThemeProvider value={NAV_THEME[colorScheme]}>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
       <Stack />
       <PortalHost />
-    </View>
+    </ThemeProvider>
   )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,18 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View } from "react-native"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Button>
-        <Text className={"text-red-500"}>Press Cmd+R to reload, </Text>
+    <View className="flex-1 w-full">
+      <Text>Edit src/app/index.tsx to edit this screen. 12312321</Text>
+      <Button variant="destructive">
+        <Text>Press Cmd+R to reload, </Text>
       </Button>
+      <Button variant="outline">
+        <Text>Cmd+D or shake for dev menu</Text>
+      </Button>
+      <Input placeholder="Input" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
